9ddeda5d48461f586aab5d6f0d1dc51beaa533f6
mdmonimul95-crypto <mdmonimul95@gmail.com>
2026-05-02 10:21:38 +0600

    project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "assets2.lottiefiles.com"],
+  },
+};
 
 export default nextConfig;
